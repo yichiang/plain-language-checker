0726fb33ff74468b7a9f509932a19eea606f6796
Brenda Vega Contreras <bvega96@outlook.com>
2023-02-18 19:45:10 -0800

    Add Text parsing classes

--- plain-language-checker/src/index.js
+++ plain-language-checker/src/index.js
@@ -15,3 +15,42 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+class Text {
+  constructor(text) {
+    this.text = text;
+    this.paragraphs = [];
+
+    // Counters
+    this.paragraphsCount = 0;
+    this.sentencesCount = 0;
+    this.wordsCount = 0;
+    this.transitionWordsCount = 0;
+    this.examplesCount = 0;
+  }
+}
+
+class Paragraph {
+  constructor(text) {
+    this.text = text;
+    this.sentences = [];
+
+    // Counters
+    this.sentencesCount = 0;
+    this.wordsCount = 0;
+    this.transitionWordsCount = 0;
+    this.examplesCount = 0;
+  }
+}
+
+class Sentence {
+  constructor(text) {
+    this.text = text;
+    this.words = [];
+
+    // Counters
+    this.wordsCount = 0;
+    this.transitionWordsCount = 0;
+    this.examplesCount = 0;
+  }
+}
