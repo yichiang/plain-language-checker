96dae8e4d454fa374bcc897b1ad7a86396a9aaf0
Arturo De la Pena Zaldivar <arturo_pspzaldivar@hotmail.com>
2023-02-19 03:20:30 -0800

    Add Feedback Classes and minor edits

--- plain-language-checker/src/index.js
+++ plain-language-checker/src/index.js
@@ -31,8 +31,10 @@ class Text {
 }
 
 class Paragraph {
-  constructor(text) {
+  constructor(text, number) {
     this.text = text;
+    // Paragraph number, starts at 1 for the first paragraph in the text
+    this.number = number;
     this.sentences = [];
 
     // Counters
@@ -44,13 +46,51 @@ class Paragraph {
 }
 
 class Sentence {
-  constructor(text) {
+  constructor(text, number) {
     this.text = text;
+    // Sentence number, starts at 1 for the first sentence in the paragraph
+    this.number = number;
     this.words = [];
+    this.feedback = [];
 
     // Counters
     this.wordsCount = 0;
     this.transitionWordsCount = 0;
     this.examplesCount = 0;
+  }
+}
+
+class Feedback {
+  constructor(name, feedbackType, link, linkText, description, matchedString, stringSuggestion = "") {
+    // The values below should be part of the metadata in the text file
+    this.name = name;
+    this.feedbackType = feedbackType; // Must be an instance of FeedbackType, see above
+    this.link = link;
+    this.linkText = linkText;
+    this.description = description;
+    // The values below are set by the parsing function
+    this.matchedString = matchedString; // String that matched a keyword
+    this.stringSuggestion = stringSuggestion; // Other word(s) suggestions, may be empty
+  }
+}
+
+class Issue extends Feedback {
+  constructor(name, link, linkText, description, stringIssue, stringSuggestion) {
+    // The values below should be part of the metadata in the text file
+    super(name, "Issue", link, linkText, description, stringIssue, stringSuggestion);
+  }
+}
+
+class Suggestion extends Feedback {
+  constructor(name, link, linkText, description, matchedString, stringSuggestion) {
+    // The values below should be part of the metadata in the text file
+    super(name, "Suggestion", link, linkText, description, matchedString, stringSuggestion);
+  }
+}
+
+class Good extends Feedback {
+  constructor(name, link, linkText, description, matchedString) {
+    // The values below should be part of the metadata in the text file
+    super(name, "Good", link, linkText, description, matchedString);
   }
 }
