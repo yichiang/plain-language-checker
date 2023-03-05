aea8f14f6a1868ac32e45726e78861ea83f22ea2
Arturo De la Pena Zaldivar <arturo_pspzaldivar@hotmail.com>
2023-03-04 23:57:52 -0800

    Convert tabs to spaces, minor fixes

--- src/Parser/Validator/SyllableCount.ts
+++ src/Parser/Validator/SyllableCount.ts
@@ -3,7 +3,7 @@ import { Suggestion } from '../Parser';
 import { Sentence } from '../Sentence';
 
 const highCount = 5;
-export default function reportSyllableHighCount(sentence: Sentence, checkForBy = false) {
+export default function reportSyllableHighCount(sentence: Sentence) {
 	const issues: {word: string, count: number}[] = [];
 	sentence.getWords().forEach(word => {
 		const count = syllable(word);
@@ -13,17 +13,16 @@ export default function reportSyllableHighCount(sentence: Sentence, checkForBy =
 	});
 	const words = issues.map(issue=> `${issue.word}`);
 	const wordList = words.join(',');
-	console.log(wordList);
 	if(issues.length > 0) {
 		sentence.getSuggestions().push(new Suggestion(
 			'Use simple words and phrases', 
 			'https://www.plainlanguage.gov/guidelines/words/use-simple-words-phrases/',
 			'Use simple words and phrases - Plain language guidelines',
-			`The sentence contains ${wordList} with high syllable count. Please consider to change if the word might be complicated.`,
+			`The sentence contains the following high-syllable words: ${wordList}.`,
 			sentence.getParagraphNumber(),
 			sentence.getSentenceNumber(),
 			wordList || '',
-			`Consider replacing "${wordList}" with simple word.`
+			`Please consider replacing "${wordList}" with simpler words.`
 		));        
 	}
 
