9c4ef7e95cdb61e2a68ca8b7d345b049cfadf002
Arturo De la Pena Zaldivar <arturo_pspzaldivar@hotmail.com>
2023-03-06 22:53:12 -0800

    Add 3000 most common words before checking syllable count

--- src/Parser/Validator/SyllableCount.ts
+++ src/Parser/Validator/SyllableCount.ts
@@ -2,15 +2,19 @@ import { syllable } from 'syllable';
 import { Suggestion } from '../Parser';
 import { Sentence } from '../Sentence';
 import { foundUrls } from './helper';
+import { commonWords } from './CommonWords';
 
 const highCount = 5;
 export default function reportSyllableHighCount(sentence: Sentence) {
 	const issues: {word: string, count: number}[] = [];
 	sentence.getWords().forEach(word => {
-		const count = syllable(word);
-		const matchUrls = foundUrls(word);
-		if((!matchUrls?.length) && count >= highCount) {
-			issues.push({word: word, count: count});
+		if (!commonWords.has(word.toLowerCase().replace(/[^\w-]/g,'')))
+		{
+			const count = syllable(word);
+			const matchUrls = foundUrls(word);
+			if((!matchUrls?.length) && count >= highCount) {
+				issues.push({word: word, count: count});
+			}
 		}
 	});
 	const words = issues.map(issue=> `${issue.word}`);
@@ -20,7 +24,7 @@ export default function reportSyllableHighCount(sentence: Sentence) {
 			'Use simple words and phrases', 
 			'https://www.plainlanguage.gov/guidelines/words/use-simple-words-phrases/',
 			'Use simple words and phrases - Plain language guidelines',
-			`The sentence contains the following high-syllable words: ${wordList}.`,
+			`The sentence contains the following uncommon, high-syllable words: ${wordList}.`,
 			sentence.getParagraphNumber(),
 			sentence.getSentenceNumber(),
 			wordList || '',
