e6b31e234ddac93a3b1ed5ac4d797137e480325b
Arturo De la Pena Zaldivar <arturo_pspzaldivar@hotmail.com>
2023-03-10 00:12:16 -0800

    Re-add header but comment it out

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import {
-	Header as CarbonHeader,
-	SkipToContent,
-	HeaderName
-} from '@carbon/react';
-import { LinkProps as RRLinkProps, Link } from 'react-router-dom';
+// import {
+// 	Header as CarbonHeader,
+// 	SkipToContent,
+// 	HeaderName
+// } from '@carbon/react';
+// import { LinkProps as RRLinkProps, Link } from 'react-router-dom';
 
 function Header(): JSX.Element {
 
 	return (
-		<CarbonHeader aria-label="Plain Language Checker">
-			<SkipToContent/>
-			<HeaderName <RRLinkProps> element={Link} to="/" prefix="Plain Language">
-				Checker
-			</HeaderName>
-		</CarbonHeader>
+		<></>
+		// <CarbonHeader aria-label="Plain Language Checker">
+		// 	<SkipToContent/>
+		// 	<HeaderName <RRLinkProps> element={Link} to="/" prefix="Plain Language">
+		// 		Checker
+		// 	</HeaderName>
+		// </CarbonHeader>
 	);
 }
 
