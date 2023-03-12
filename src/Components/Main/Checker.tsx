33bac26d518e79809a6c70e694de6fc205136026
Arturo De la Pena Zaldivar <arturo_pspzaldivar@hotmail.com>
2023-03-12 03:06:44 -0700

    Remove default text

--- src/Components/Main/Checker.tsx
+++ src/Components/Main/Checker.tsx
@@ -6,26 +6,7 @@ import {
 import './Checker.scss';
 
 function Checker(props: CheckerPropsType): JSX.Element {
-	//ToDo:// remove default context after developemnt
-	const [inputContent, setInputContent] = useState<string>(`
-	The Americans with Disabilities Act of 1990 or ADA is a civil rights law that prohibits discrimination based on disability.
-
-	ADA is important. We need to know the impact on BLV users.
-
-
-	Use this link to verify the plain language usage: https://yichiang.github.io/plain-language-checker/#/.
-
-	The report found that people living in countries with liberal democratic governments are “more likely to worry about misinformation than people in countries without or with limited democratic institutions,” and that fake news concerns were higher among people with higher education levels. 
-
-	He was withheld while we were being fed. New regulations were proposed.	
-		
-	Statistical classification is the process of assigning observations to a category or set of labels. This is a basic problem in statistics, science and artificial intelligence. For example, a self-driving car that needs to decide if a moving object is a pedestrian, car, bicycle or other entity such as leaves being blown by the wind. An algorithm that performs statistical classification is known as a classifier. However, I don't think finding these solutions means an end to all our troubles.
-	
-	A sailor's life is indeed a hard life.
-	
-	In the 1920s the renowned English scholar H.W. Fowler dismissed and/or as an “ugly device” that may be “common and convenient in some kinds of official, legal, and business documents.
-	`);
-
+	const [inputContent, setInputContent] = useState<string>('');
 	const onTextInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
 		const target = (event as SyntheticEvent).target;
 		setInputContent((target as HTMLInputElement).value);
